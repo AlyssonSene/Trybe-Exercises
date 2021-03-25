d385f426bb809fb9bdb5402731acc80086a5a0ec
AlyssonSene <alysson_silva@outlook.com>
2021-03-25 20:11:29 -0300

    exercicios de comparaçao

---
+++ exercises/bloco_4/dia_1/compara_2_numeros.js
@@ -0,0 +1,10 @@
+var n1 = 5;
+var n2 = 5;
+
+if (n1===n2){
+    console.log('Os números são iguais');
+} else if (n1>n2){
+    console.log('O primeiro número é maior');
+}else if (n1<n2){
+    console.log('O segundo número é maior');
+}
