17721616f0195aa61a93c0986b1b771fe4ae7e70
AlyssonSene <alysson_silva@outlook.com>
2021-03-26 19:23:05 -0300

    verifica se numero e par ou impar

---
+++ exercises/bloco_4/dia_1/numImpar.js
@@ -0,0 +1,13 @@
+var num1 = 4;
+var num2 = 4;
+var num3 = 5;
+let impar;
+
+if (num1 % 2 === 1 || num2 % 2 === 1 || num3 % 2 === 1) {
+    impar = true;
+    console.log(impar);
+}
+else {
+    impar = false;
+    console.log(impar);
+}
