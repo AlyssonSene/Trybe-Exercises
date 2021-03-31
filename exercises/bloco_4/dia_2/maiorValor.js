108f37a7f71c146618597e039ea6859598b7e9ea
AlyssonSene <alysson_silva@outlook.com>
2021-03-31 15:39:57 -0300

    corrigindo exercicios

--- exercises/bloco_4/dia_2/maiorValor.js
+++ exercises/bloco_4/dia_2/maiorValor.js
@@ -1,9 +1,7 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27, 150];
 let higherNumber = 0;
 for (index = 0; index < numbers.length; index +=1) {
 	if (higherNumber < numbers[index]){
 		higherNumber = numbers[index];
 	}
 }console.log(higherNumber);
-
-    
