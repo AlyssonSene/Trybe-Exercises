8c30a11d32e3929fb62da3755568e75267bb6aa9
AlyssonSene <alysson_silva@outlook.com>
2021-03-31 16:29:09 -0300

    finalizando exercicios bonus

--- exercises/bloco_4/dia_2/bonus/bonus2.js
+++ exercises/bloco_4/dia_2/bonus/bonus2.js
@@ -1,7 +1,7 @@
 let numbers = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
 for (index = 1; index < numbers.length; index += 1) {
   for (secondIndex = 0; secondIndex < index; secondIndex +=1){
-   if (numbers[index] < numbers[secondIndex]){
+   if (numbers[index] > numbers[secondIndex]){
       let position = numbers[index];
       numbers[index] = numbers[secondIndex];
       numbers[secondIndex] = position;
