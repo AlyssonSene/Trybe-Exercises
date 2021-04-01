d9ad4821e0881eb3957b36ec76fa6742c792cbdd
AlyssonSene <alysson_silva@outlook.com>
2021-04-01 17:40:27 -0300

    exercicios de funçoes do dia 4

---
+++ exercises/bloco_4/dia_4/funções/exer1.js
@@ -0,0 +1,13 @@
+let word = 'socorrammesubinoonibusemmarrocos';
+let aux;
+
+function verificaPalindrome(word) {
+  aux = word.split('').reverse().join('');
+  if (aux === word) {
+    return true;
+  } else {
+    return false;
+  }
+}
+verificaPalindrome(word);
+console.log(verificaPalindrome(word));
