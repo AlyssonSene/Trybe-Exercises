8c5a81a1c277a1dee3b694ce58b42ada4013ddd4
AlyssonSene <alysson_silva@outlook.com>
2021-04-08 09:46:49 -0300

    primeiro exercicio com DOM

---
+++ exercises/bloco_5/script.js
@@ -0,0 +1,12 @@
+document.getElementById("footer-container").style.backgroundColor = "rgb(0, 176, 105)";
+document.getElementById("header-container").style.backgroundColor = "rgb(0, 176, 105)";
+document.getElementsByClassName("emergency-tasks")[0].style.backgroundColor = "rgb(249, 219, 94)";
+document.getElementsByClassName("no-emergency-tasks")[0].style.backgroundColor = "rgb(255,159, 132)";
+document.getElementsByTagName("h3")[0].style.backgroundColor = "rgb(165, 0, 243)";
+document.getElementsByTagName("h3")[1].style.backgroundColor = "rgb(165, 0, 243)";
+document.getElementsByTagName("h3")[2].style.backgroundColor = "black";
+document.getElementsByTagName("h3")[3].style.backgroundColor = "black";
+
+
+
+  
