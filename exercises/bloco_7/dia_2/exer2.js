b39c31a9db483b19c4cb7a514008a65ca95b11b7
AlyssonSene <alysson_silva@outlook.com>
2021-04-30 18:28:10 -0300

    exercicio 6

--- exercises/bloco_7/dia_2/exer2.js
+++ exercises/bloco_7/dia_2/exer2.js
@@ -40,3 +40,18 @@ for(index in pairKeyValue) {
   console.log('País:', pairKeyValue[index][0]);
   console.log('Capital:', pairKeyValue[index][1]);
 };
+
+
+
+const person = {
+  name:'Roberto',
+};
+
+const lastName = {
+  lastName: 'Silva',
+};
+
+const newPerson = Object.assign({},person,lastName);
+newPerson.name = 'Gilberto';
+console.log(newPerson);
+console.log(person);
