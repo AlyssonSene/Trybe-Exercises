920cf2827add35dd063d9aa0af0108d6f2d3c876
AlyssonSene <alysson_silva@outlook.com>
2021-05-11 18:31:44 -0300

    exemplos e exercicios 1,2 bloco 8 dia 5

--- exercises/bloco_8/dia_5/exemplo1.js
+++ exercises/bloco_8/dia_5/exemplo1.js
@@ -19,14 +19,14 @@
 
 // console.log(fruitSalad(specialFruit, additionalItens));
 
-const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
+// const saudacoes = ['Olá', (saudacao) => console.log(saudacao)];
 
-saudacoes[1](saudacoes[0]); // Olá
+// saudacoes[1](saudacoes[0]); // Olá
 
 // Produza o mesmo resultado acima, porém utilizando array destructuring
-const [saudacao, realizaSaudacao] = saudacoes;
+// const [saudacao, realizaSaudacao] = saudacoes;
 
-console.log(realizaSaudacao(saudacao));
+// console.log(realizaSaudacao(saudacao));
 
 
 
@@ -34,14 +34,80 @@ console.log(realizaSaudacao(saudacao));
 // let animal = 'água';
 // let bebida = 'arroz';
 
-// // Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
+// Utilizando array destructuring, faça com que os valores apareçam nas variáveis correspondentes ao seu verdadeiro tipo
 // [comida, bebida, animal] = [ bebida, animal, comida];
 
 // console.log(comida, animal, bebida); // arroz gato água
 
 // let numerosPares = [1, 3, 5, 6, 8, 10, 12];
 
-// // Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
+// Utilize array destructuring para produzir o resultado esperado pelo console.log abaixo
 // [,,, ...numerosPares] = numerosPares;
 
 // console.log(numerosPares); // [6, 8, 10, 12]
+
+// const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+// const person = {
+//     firstName: 'João',
+//     lastName: 'Jr II',
+// };
+
+// const otherPerson = {
+//     firstName: 'Ivan',
+//     lastName: 'Ivanovich',
+//     nationality: 'Russian',
+// };
+
+// const { nationality = 'Brazilian'} = person;
+
+// console.log(getNationality(otherPerson)); // Ivan is Russian
+// console.log(getNationality(person));
+
+// const newUser = (id, name, email) => {
+//   return {
+//     id,
+//     name,
+//     email,
+//   };
+// };
+
+// console.log(newUser(54, 'isabella', 'isabella@email.com')); // { id: 54, name: 'isabella', email: 'isabella@email.com' }
+
+// const getPosition = (latitude, longitude) => ({
+//   latitude,
+//   longitude,
+// });
+
+// console.log(getPosition(-19.8157, -43.9542));
+
+// const multiply = (number, value = 1) => {
+//   return number * value;
+// };
+
+// console.log(multiply(8, 5));
+
+// const assert = require('assert');
+
+// const rectangleArea = (width, height) => width * height;
+
+// const rectangle1 = [1, 2];
+// const rectangle2 = [3, 5];
+// const rectangle3 = [6, 9];
+// const rectangles = [rectangle1, rectangle2, rectangle3];
+
+// rectangles.forEach((rectangle) => {
+//   assert.strictEqual(rectangleArea(...rectangle), rectangle[0] * rectangle[1]); // altere a chamada da funcao rectangleArea
+// });
+
+
+const assert = require('assert');
+
+const sum = (...numbers) => numbers.reduce(((acc, val) => acc + val), 0);
+
+
+assert.strictEqual(sum(), 0);
+assert.strictEqual(sum(1), 1);
+assert.strictEqual(sum(1, 2), 3);
+assert.strictEqual(sum(1, 2, 3), 6);
+assert.strictEqual(sum(1, 2, 3, 4), 10);
