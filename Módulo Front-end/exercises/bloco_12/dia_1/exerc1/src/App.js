3295654290fce18280a463513c42f0a89749425c
AlyssonSene <alysson_silva@outlook.com>
2021-06-18 19:13:13 -0300

    exercicio dia 1 bloco 12

---
+++ Módulo Front-end/exercises/bloco_12/dia_1/exerc1/src/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super()
+      this.state = {
+        numeroDeClicks: 0,
+      }
+
+      this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick = () => {
+    /* Passando uma callback à função setState, que recebe de parâmetros
+    o estado anterior e as props do componente, você garante que as atualizações
+    do estado acontecerão uma depois da outra! */
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeClicks: estadoAnterior.numeroDeClicks + 1
+    }))
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <p className="clicks">{this.state.numeroDeClicks}</p>
+        <button onClick={this.handleClick} className="button">Aperte aqui </button>
+      </div>
+    )
+  }
+}
+
+export default App
